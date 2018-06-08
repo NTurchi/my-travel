75b37415cb44e06d9be0121f4892e6ba2f6a5f2a
AugusteD <auguste.dupont@epsi.fr>
2018-06-08 15:01:18 +0200

    Ajout circuit etape

---
+++ src/app/travel/admin/cruds/crud-circuit/crud-circuit-etape.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons, NgbDatepickerConfig, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-crud-circuit',
+  template: ``
+})
+export class CrudCircuitEtapeComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
+}
